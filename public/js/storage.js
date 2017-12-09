1391ffe501db2c805d8f19b8a9685100e8aa5a3b
Tessa Kruger <tessarfoto@gmail.com>
2017-12-09 18:51:13 -0500

    Intro overlay doesn't appear if localStorage has key

--- public/js/storage.js
+++ public/js/storage.js
@@ -1,11 +1,13 @@
 $(document).ready(function (){
-	if (localStorage.getItem("visited")) {
-		$('#intro--overlay').hide();
-	} else {
-		$('#intro--overlay').fadeIn();
+	if (!localStorage.getItem("visited")) {
+		$('#intro--overlay').show();
 		//Set the key
 	    localStorage.setItem("visited", "true");
+	    $('#content').css('opacity', '1');
+	} else {
+		$('#content').css('opacity', '1');
 	}
+	
 
 	//clear localStorage on tab close
 	// window.onbeforeunload = function() {
