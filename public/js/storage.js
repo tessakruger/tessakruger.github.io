0a0527e77e0d510b6d66f0ccf1a24c4c20e07364
Tessa Kruger <tessarfoto@gmail.com>
2017-12-09 18:33:44 -0500

    Check local storage on doc load

--- public/js/storage.js
+++ public/js/storage.js
@@ -1,12 +1,14 @@
-if (localStorage.getItem("visited")) {
-	$('#intro--overlay').hide();
-} else {
-	$('#intro--overlay').fadeIn();
-	//Set the key
-    localStorage.setItem("visited", "true");
-}
+$(document).ready(function (){
+	if (localStorage.getItem("visited")) {
+		$('#intro--overlay').hide();
+	} else {
+		$('#intro--overlay').fadeIn();
+		//Set the key
+	    localStorage.setItem("visited", "true");
+	}
 
-//clear localStorage on tab close
-window.onbeforeunload = function() {
-    localStorage.removeItem("visited");
-};
+	//clear localStorage on tab close
+	window.onbeforeunload = function() {
+	    localStorage.removeItem("visited");
+	};
+})
