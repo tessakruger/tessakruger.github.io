b188949b9799fbc65dcb139be6957cc3ec51a7f8
Tessa Kruger <tessarfoto@gmail.com>
2017-12-11 14:42:05 -0500

    Page bg should match post's bg

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,9 +1,18 @@
 $(document).ready(function (){
 	var nav = $( "#nav" );
+	var bg = $( "#post-nav" );
 	nav.hide();
-		$('#hoverdown').hover(function(){
-			$(this).find(nav).stop(true, true).delay(200).fadeIn(200);
-		}, function() {
-			$(this).find(nav).stop(true, true).delay(200).fadeOut(200);
-		})
+	var post = $('.wrapper--left-side.fixed.left-post');
+	var color = post.css("backgroundColor");
+	$( '#content' ).css('background', color);
+	$('#hoverdown').hover(function(){
+		$(this).find(nav).stop(true, true).delay(200).slideDown(300);
+		$(this).parent(bg).css('background', color);
+	}, function() {
+		$(this).find(nav).stop(true, true).delay(200).slideUp(300);
+		$(this).parent(bg).delay(600).queue(function (next) {
+			$(this).css('background', 'none');
+			next();
+		});
+	})
 })
