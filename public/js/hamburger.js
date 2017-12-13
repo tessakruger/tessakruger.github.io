60576de3c9350307b4ac574313f985df7552d89f
Tessa Kruger <tessarfoto@gmail.com>
2017-12-12 20:23:34 -0500

    Fix nav layout on mobile on all pages

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,30 +1,23 @@
 
 
 
-$(document).ready(function (){
- 	var nav = $( "#nav" );
- 	nav.hide();
- 		$('#hoverdown').hover(function(){
- 			$(this).find(nav).stop(true, true).delay(200).slideDown(200);
- 		}, function() {
- 			$(this).find(nav).stop(true, true).delay(0).slideUp(200);
- 		})
-})
 
-// $(document).ready(function (){
-// 	var nav = $( "#nav" );
-// 	var bg = $( "#post-nav" );
-// 	nav.hide();
-// 	var post = $('.wrapper--left-side.fixed.left-post');
-// 	var color = post.css("backgroundColor");
-// 		$( '#content' ).css('background', color);
-// 		$('#hoverdown').hover(function(){
-// 			$(this).find(nav).stop(true, true).delay(200).slideDown(300);
-// 			$(this).parent(bg).css('background', color);
-// 		}, function() {
-// 			$(this).find(nav).stop(true, true).delay(200).slideUp(300);
-// 			$(this).parent(bg).delay(600).queue(function (next) {
-// 				$(this).css('background', 'none');
-// 				next();
-// 		});
-// })
+
+$(document).ready(function (){
+	var nav = $( "#nav" );
+	var bg = $( "#post-nav" );
+	nav.hide();
+	var post = $('.wrapper--left-side.fixed.left-post');
+	var color = post.css("backgroundColor");
+	$( '#content' ).css('background', color);
+	$('#hoverdown').hover(function(){
+		$(this).find(nav).stop(true, true).delay(200).slideDown(300);
+		$(this).parent(bg).css('background', none);
+	}, function() {
+		$(this).find(nav).stop(true, true).delay(200).slideUp(300);
+		$(this).parent(bg).delay(600).queue(function (next) {
+			$(this).css('background', 'color');
+			next();
+		});
+	})
+})
