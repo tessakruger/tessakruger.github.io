7c10710b0e4f609334e20d57fadbdb38a07e3ad9
Tessa Kruger <tessarfoto@gmail.com>
2017-12-12 19:09:37 -0500

    Don't change nav bg acc to project asset bg

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,18 +1,30 @@
+
+
+
 $(document).ready(function (){
-	var nav = $( "#nav" );
-	var bg = $( "#post-nav" );
-	nav.hide();
-	var post = $('.wrapper--left-side.fixed.left-post');
-	var color = post.css("backgroundColor");
-	$( '#content' ).css('background', color);
-	$('#hoverdown').hover(function(){
-		$(this).find(nav).stop(true, true).delay(200).slideDown(300);
-		$(this).parent(bg).css('background', color);
-	}, function() {
-		$(this).find(nav).stop(true, true).delay(200).slideUp(300);
-		$(this).parent(bg).delay(600).queue(function (next) {
-			$(this).css('background', 'none');
-			next();
-		});
-	})
+ 	var nav = $( "#nav" );
+ 	nav.hide();
+ 		$('#hoverdown').hover(function(){
+ 			$(this).find(nav).stop(true, true).delay(200).slideDown(200);
+ 		}, function() {
+ 			$(this).find(nav).stop(true, true).delay(0).slideUp(200);
+ 		})
 })
+
+// $(document).ready(function (){
+// 	var nav = $( "#nav" );
+// 	var bg = $( "#post-nav" );
+// 	nav.hide();
+// 	var post = $('.wrapper--left-side.fixed.left-post');
+// 	var color = post.css("backgroundColor");
+// 		$( '#content' ).css('background', color);
+// 		$('#hoverdown').hover(function(){
+// 			$(this).find(nav).stop(true, true).delay(200).slideDown(300);
+// 			$(this).parent(bg).css('background', color);
+// 		}, function() {
+// 			$(this).find(nav).stop(true, true).delay(200).slideUp(300);
+// 			$(this).parent(bg).delay(600).queue(function (next) {
+// 				$(this).css('background', 'none');
+// 				next();
+// 		});
+// })
